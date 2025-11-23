a581d07f02aa5844ad5cd9bd170cec4c4de455f7
defix-dev <kostikmelnik7778@gmail.com>
2025-11-23 23:12:56 +0300

    docker added

---
+++ src/main/resources/db/migration/V2__Insert_roles.sql
@@ -0,0 +1,4 @@
+INSERT INTO roles (id, name) VALUES
+                                 (1, 'USER'),
+                                 (2, 'MODERATOR'),
+                                 (3, 'ADMIN');
